ddc65877c4338651cb4d0da2c53589b333cdf240
yashsinghal98 <68867021+yashsinghal98@users.noreply.github.com>
2020-10-20 14:12:55 +0530

    changes

--- src/components/circle.js
+++ src/components/circle.js
@@ -1,7 +1,13 @@
 // import 
-
+import Shape from "./shape";
 // declare class
-
+export default class Circle extends Shape{
+  constructor(color)
+  {
+  super(color);
+  }
+  calculateArea=function(){};
+}
 
 // export
 
--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,10 @@
 // declare class
-
+export default class Shape{
+  constructor(color)
+  {
+    this.color=color;
+  }
+  drawShape=function (){};
+  calculateArea=function (){};
+}
 // export class
